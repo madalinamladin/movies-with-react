a58730a724aeb8b10e60398bfdd4e05b3e66c97b
Madalina Mladin <mladinmih@yahoo.com>
2017-06-03 13:15:03 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
 import React, { Component } from 'react';
 
+var movies = [
+    {title: 'Life of Pi', year: 2012},
+    {title: 'Brokeback Mountain', year: 2005},
+    {title: 'Crouching Tiger, Hidden Dragon', year: 2000},
+    {title: 'The Wedding Banquet', year: 1993}
+  ];
+
 class App extends Component {
 
     render() {
         return (
             <div>
-                <p>Hello World</p>
+                <h1>Movies with React</h1>
+                <h2>List of Movies</h2>
+                <div className="search">
+                	<div className="search-label">Looking for a specific movie?</div>
+                	<input type="text" onChange={this.handleChange} id="txt_search" name="txt_search" placeholder="Search.." className="search-textinput"/>
+                </div>
+                {movies.map((movie, i) => <MovieRow key = {i} data = {movie} />)}
             </div>
         );
     }
 }
+
+class MovieRow extends React.Component {
+	   render() {
+	      return (
+	         <div className="movie">
+                  <div className="movie-name">{this.props.data.title}</div>
+                  <div className="movie-year">Year: {this.props.data.year}</div>
+             </div>
+	      );
+	   }
+	}
+
 
 export default App;
