5e937c26a32a4e9d2a37f73da7b6116fc2843198
Madalina Mladin <mladinmih@yahoo.com>
2017-06-07 17:21:13 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 
 var movies = [
+	{title: 'Up', year: 2009},
+	{title: 'A Series of Unfortunate Events', year: 2004},
+	{title: 'The Prestige', year: 2006},
+	{title: 'Prometheus', year: 2012},
+	{title: 'Shrek', year: 2001},
+	{title: '50/50', year: 2011},
+	{title: 'Salt', year: 2010},
+    {title: 'World War Z', year: 2013},
     {title: 'Life of Pi', year: 2012},
     {title: 'Brokeback Mountain', year: 2005},
     {title: 'Crouching Tiger, Hidden Dragon', year: 2000},
     {title: 'The Wedding Banquet', year: 1993}
   ];
+
+  var moviesFiltered = [];
 
 class App extends Component {
 
@@ -16,12 +27,35 @@ class App extends Component {
                 <h2>List of Movies</h2>
                 <div className="search">
                 	<div className="search-label">Looking for a specific movie?</div>
-                	<input type="text" onChange={this.handleChange} id="txt_search" name="txt_search" placeholder="Search.." className="search-textinput"/>
+                	<input type="text" onChange={filterMovies} id="txt_search" name="txt_search" placeholder="Search.." className="search-textinput"/>
                 </div>
-                {movies.map((movie, i) => <MovieRow key = {i} data = {movie} />)}
+                <div id="movies">
+                	{movies.map((movie, i) => <MovieRow key = {i} data = {movie} />)}
+                </div>
             </div>
         );
     }
+}
+
+function filterMovies() {
+	    
+	 var keyword = document.getElementById("txt_search").value.toLowerCase();
+	 if(keyword.length>0){
+	 	moviesFiltered = [];
+	 	var i = 0;
+	 	for(i=0;i<movies.length;i++){
+	 		var title = movies[i]['title'].toLowerCase();
+	 		if(title.search(keyword)>-1){
+	 			moviesFiltered.push(movies[i]);
+	 		}
+	 	}
+	 }else{
+	 	moviesFiltered = movies;
+	 }
+	 ReactDOM.render(
+				    <Clock />,
+				    document.getElementById('movies')
+				  );
 }
 
 class MovieRow extends React.Component {
@@ -35,5 +69,15 @@ class MovieRow extends React.Component {
 	   }
 	}
 
+
+class Clock extends React.Component {
+  render() {
+    return (
+      <div>
+        {moviesFiltered.map((movie, i) => <MovieRow key = {i} data = {movie} />)}
+      </div>
+    );
+  }
+}
 
 export default App;
